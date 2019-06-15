0ea27547dba39d44c9462eaab0a3d7b1c589d89f
johnmwangi <jonesmwas356@gmail.com>
2019-06-15 13:51:33 +0300

    changes made to the setting file

--- portapp/settings.py
+++ portapp/settings.py
@@ -43,8 +43,30 @@ DEBUG = True
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
-# Application definition
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+
+
+# Application definition
 INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework',
